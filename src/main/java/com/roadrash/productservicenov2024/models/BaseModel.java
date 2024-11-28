74d55d494202cc01d1046fafea151713b7dac583
roadrashinfinity <rudreshgawas1444@gmail.com>
2024-11-28 18:19:41 +0530

    base model changes

--- src/main/java/com/roadrash/productservicenov2024/models/BaseModel.java
+++ src/main/java/com/roadrash/productservicenov2024/models/BaseModel.java
@@ -5,11 +5,17 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @MappedSuperclass
 public class BaseModel {
     @Id
     private long id;
+    private Date created_at;
+    private Date updated_at;
+    private boolean deleted;
+
 
 }
